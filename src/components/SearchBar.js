92e8f186164d10e143af8a14d4d90467d2caf5a2
jeff.smith <jeff.smith@usaa.com>
2019-08-29 11:38:45 -0500

    added controlled input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+state = { term: ''}
+
+
     render(){
+        
+        onInputChange = event => {
+            this.setState({ term: event.target.value });
+        };
+
+        onFormSubmit = event => {
+            event.prevent.default();
+            
+        };
+
         return(
-            <div>Search Bar</div>
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <label>Video Search</label>
+                    <input 
+                        type="text" 
+                        value={this.state.term}
+                        onChange={this.onInputChange}/>
+                </form>
+            </div>
         )
-    }
+    };
 }
 
 export default SearchBar;
