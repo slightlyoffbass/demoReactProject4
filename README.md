2da59651d79df4bbc9ca2c0507fbfd63f8dc3bbf
jeff.smith <slightlyoffbass@gmail.com>
2019-08-29 14:43:35 -0500

    fix params issue

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 ## Add axios
 npm install --save axios
 
+"https://github.com/axios/axios/issues/2230"
+ - issue, params were not appending with axios.create. 
+ - reverted axios back to version 17
+
+
 ## Youtube 
--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import React from 'react';
 import SearchBar from './components/SearchBar';
 import youtube from '../src/api/youtube';
 
-class App extends React.Component{
+class App extends React.Component {
 
-onTermSubmit = term =>{
-    youtube.get('/search',{
-        params:{
-            q: term
-        }
-    })
-};
+    onTermSubmit = term =>{
+        youtube.get('/search', {
+            params: {
+                q: term
+            }
+        });
+    };
 
-render(){
-    return (
-        <div className="ui container">
-            <SearchBar onFormSubmit={this.onTermSubmit}/>
-        </div>
-    )
-}
+    render(){
+        return (
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit}/>
+            </div>
+        )
+    }
 };
 
 export default App;
